3c9ef77c5c90f7ea6d7c44bf5a60fb76a9ba0729
RenjithKI <renjith.kachappilly@gmail.com>
2018-06-02 16:42:19 +0100

    first commit

---
+++ src/main/java/com/renjith/bootProjectA3Three/ReaderRepository.java
@@ -0,0 +1,6 @@
+package com.renjith.bootProjectA3Three;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository extends JpaRepository<Reader, String> {
+}
